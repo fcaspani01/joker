4c151b213bc547963c57ceea09ecc5367db53ef1
Francesco Caspani <francesco.caspani@serioplast.com>
2021-03-26 12:00:09 +0100

    feature/SERP-964: - Added csv logging

--- templates/user_data.sh
+++ templates/user_data.sh
@@ -20,9 +20,9 @@ sudo bash -c 'cat >> /etc/docker/daemon.json <<EOL
 EOL'
 
 ## POST INSTALL
-#sudo usermod -aG docker $USER
-#newgrp docker
-#sudo curl -L "https://github.com/docker/compose/releases/download/1.28.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-#sudo chmod +x /usr/local/bin/docker-compose
-#sudo curl -L https://raw.githubusercontent.com/docker/compose/1.28.0/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
-#sudo service docker restart
+sudo usermod -aG docker $USER
+newgrp docker
+sudo curl -L "https://github.com/docker/compose/releases/download/1.28.6/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo curl -L https://raw.githubusercontent.com/docker/compose/1.28.6/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
+sudo service docker restart
