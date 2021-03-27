d032e20949418043bae37372e75e8c9feb37431d
Francesco Caspani <francesco.caspani@serioplast.com>
2021-03-27 15:19:29 +0100

    user_data.sh updated

--- templates/user_data.sh
+++ templates/user_data.sh
@@ -7,7 +7,13 @@ sudo echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get -y update
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io
-sudo apt-get -y install nmap
+sudo apt-get -y install netcat
+## POST INSTALL
+sudo usermod -aG docker admin
+newgrp docker
+sudo curl -L "https://github.com/docker/compose/releases/download/1.28.6/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo curl -L https://raw.githubusercontent.com/docker/compose/1.28.6/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
 sudo touch /etc/docker/daemon.json
 sudo bash -c 'cat >> /etc/docker/daemon.json <<EOL
 {
@@ -18,11 +24,4 @@ sudo bash -c 'cat >> /etc/docker/daemon.json <<EOL
 }
 }
 EOL'
-
-## POST INSTALL
-sudo usermod -aG docker $USER
-newgrp docker
-sudo curl -L "https://github.com/docker/compose/releases/download/1.28.6/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-sudo curl -L https://raw.githubusercontent.com/docker/compose/1.28.6/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
 sudo service docker restart
